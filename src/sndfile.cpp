83fb6c82be8e7c793150cf894745939638427dac
Konstanty Bialkowski <konstanty@ieee.org>
2013-08-13 21:29:29 +1000

    Use bit-test for m-nType.
    --Patch from Ozkan Sezer <sezero@users.sf.net>
    Note that m_nType should only be set to a single "type". 
    
    Signed-off-by: Konstanty Bialkowski <konstanty@ieee.org>

--- src/sndfile.cpp
+++ src/sndfile.cpp
@@ -770,11 +770,11 @@ UINT CSoundFile::GetSaveFormats() const
 {
 	UINT n = 0;
 	if ((!m_nSamples) || (!m_nChannels) || (m_nType == MOD_TYPE_NONE)) return 0;
-	switch(m_nType)
-	{
-	case MOD_TYPE_MOD:	n |= MOD_TYPE_MOD;
-	case MOD_TYPE_S3M:	n |= MOD_TYPE_S3M;
-	}
+	if (m_nType & MOD_TYPE_MOD)
+		n |= MOD_TYPE_MOD;
+	if (m_nType & MOD_TYPE_S3M)
+		n |= MOD_TYPE_S3M;
+	// Can always save to XM & IT
 	n |= MOD_TYPE_XM | MOD_TYPE_IT;
 	if (!m_nInstruments)
 	{
