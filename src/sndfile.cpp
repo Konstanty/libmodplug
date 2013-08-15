23f3d68f1e254a6d4c71e63adccdf143bd85d3a1
Konstanty Bialkowski <konstanty@ieee.org>
2013-08-16 09:01:30 +1000

    Prevent API call from getting non existant sample.
    Reported by Axel "0vercl0k" Souchet & Florian "Agix" Gaultier

--- src/sndfile.cpp
+++ src/sndfile.cpp
@@ -789,7 +789,8 @@ UINT CSoundFile::GetSampleName(UINT nSample,LPSTR s) const
 //--------------------------------------------------------
 {
         char sztmp[40] = "";      // changed from CHAR
-	memcpy(sztmp, m_szNames[nSample],32);
+	if (nSample < MAX_SAMPLES)
+		memcpy(sztmp, m_szNames[nSample], 32);
 	sztmp[31] = 0;
 	if (s) strcpy(s, sztmp);
 	return strlen(sztmp);
