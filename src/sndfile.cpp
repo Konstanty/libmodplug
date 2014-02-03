69e10dfde46da53ec751baeb423b2e8f98f864f9
Konstanty Bialkowski <konstanty@ieee.org>
2014-02-03 13:42:25 +1000

    Remove old comments regarding CHAR->char change.

--- src/sndfile.cpp
+++ src/sndfile.cpp
@@ -1762,7 +1762,7 @@ void CSoundFile::CheckCPUUsage(UINT nCPU)
 BOOL CSoundFile::SetPatternName(UINT nPat, LPCSTR lpszName)
 //---------------------------------------------------------
 {
-        char szName[MAX_PATTERNNAME] = "";   // changed from CHAR
+        char szName[MAX_PATTERNNAME] = "";
 	if (nPat >= MAX_PATTERNS) return FALSE;
 	if (lpszName) lstrcpyn(szName, lpszName, MAX_PATTERNNAME);
 	szName[MAX_PATTERNNAME-1] = 0;
@@ -1771,7 +1771,7 @@ BOOL CSoundFile::SetPatternName(UINT nPat, LPCSTR lpszName)
 	{
 		if (!lpszName[0]) return TRUE;
 		UINT len = (nPat+1)*MAX_PATTERNNAME;
-		char *p = new char[len];   // changed from CHAR
+		char *p = new char[len];
 		if (!p) return FALSE;
 		memset(p, 0, len);
 		if (m_lpszPatternNames)
