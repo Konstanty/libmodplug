4c7b7f7475c3d20d2df213c88a9d5bd0bd3d406a
jarikomppa <jari.komppa@gmail.com>
2014-05-16 19:59:03 +0300

    allocated dynamic temps from heap instead of stack to get to compile with msvc2010

--- src/load_abc.cpp
+++ src/load_abc.cpp
@@ -2252,11 +2252,11 @@ static void abc_preprocess(ABCHANDLE *h, ABCMACRO *m)
 	if( m->n ) {
 		k = m->n - m->name;
 		for( i=0; i<14; i++ ) {
-			char t[strlen(m->name) + 1];
+			char *t = new char[strlen(m->name) + 1];
 			strcpy(t, m->name);
 			t[k] = "CDEFGABcdefgab"[i];
 			l = strlen(m->subst);
-			char s[2 * l + 1];
+			char *s = new char[2 * l + 1];
 			char *p = s;
 			for( j=0; j<l; j++ ) {
 				a = m->subst[j];
@@ -2273,6 +2273,8 @@ static void abc_preprocess(ABCHANDLE *h, ABCMACRO *m)
 			}
 			*p = '\0';
 			abc_substitute(h, t, s);
+			delete[] s;
+			delete[] t;
 		}
 	}
 	else
