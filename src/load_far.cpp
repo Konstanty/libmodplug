d0adf30a36fd5e40c9757a9c458dbacdd506e5c4
Konstanty <konstanty@ieee.org>
2017-06-02 23:35:49 +1000

    FAR: out by one on check

--- src/load_far.cpp
+++ src/load_far.cpp
@@ -140,7 +140,7 @@ BOOL CSoundFile::ReadFAR(const BYTE *lpStream, DWORD dwMemLength)
 		const BYTE *p = lpStream + dwMemPos + 2;
 		UINT max = rows*16*4;
 		if (max > patlen-2) max = patlen-2;
-		if (dwMemPos + max > dwMemLength - 4) return TRUE;
+		if (dwMemPos + max > dwMemLength - 4 - 1) return TRUE;
 		for (UINT len=0; len<max; len += 4, m++)
 		{
 			BYTE note = p[len];
