083e66232a66c08018c3ff1d93e42530126f1044
Konstanty Bialkowski <konstanty@ieee.org>
2019-08-11 17:43:29 +1000

    MED: Ensure that there is room to read one double

--- src/load_med.cpp
+++ src/load_med.cpp
@@ -662,7 +662,7 @@ BOOL CSoundFile::ReadMed(const BYTE *lpStream, DWORD dwMemLength)
 			}
 			UINT pseq = 0;
 
-			if ((playseqtable) && (playseqtable < dwMemLength) && (nplayseq*(4+1) < dwMemLength - playseqtable))
+			if ((playseqtable) && (playseqtable < dwMemLength - 4) && (nplayseq*(4+1) < dwMemLength - playseqtable))
 			{
 				pseq = bswapBE32(((LPDWORD)(lpStream+playseqtable))[nplayseq]);
 			}
