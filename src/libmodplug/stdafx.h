cf6f663f8ab640c805197499b0a1e6fb07102bf9
Ozkan Sezer <sezeroz@gmail.com>
2022-05-05 11:11:02 +0300

    specifically use ansi versions of lstrcpy, lstrcpyn and wsprintf

--- src/libmodplug/stdafx.h
+++ src/libmodplug/stdafx.h
@@ -96,9 +96,9 @@ typedef const char* LPCSTR;
 typedef void* PVOID;
 typedef void VOID;
 
-#define lstrcpyn strncpy
-#define lstrcpy strcpy
-#define wsprintf sprintf
+#define lstrcpynA strncpy
+#define lstrcpyA strcpy
+#define wsprintfA sprintf
 
 #define WAVE_FORMAT_PCM 1
 
